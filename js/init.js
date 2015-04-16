a8f7443ee621a1d4a8e066386f439f6309255a4f
Nicholas Moreles <nicholas.moreles@utexas.edu>
2015-04-16 02:45:09 -0500

    change resolution boundary for  transition from desktop to mobile

--- js/init.js
+++ js/init.js
@@ -10,9 +10,9 @@
 		reset: 'full',
 		breakpoints: {
 			'global':	{ range: '*', href: 'css/style.css' },
-			'desktop':	{ range: '737-', href: 'css/style-desktop.css', containers: 1200, grid: { gutters: 50 } },
-			'1000px':	{ range: '737-1200', href: 'css/style-1000px.css', containers: 960, grid: { gutters: 25 }, viewport: { width: 1024 } },
-			'mobile':	{ range: '-736', href: 'css/style-mobile.css', containers: '100%!', grid: { collapse: true, gutters: 20 }, viewport: { scalable: false } }
+			'desktop':	{ range: '1024-', href: 'css/style-desktop.css', containers: 1200, grid: { gutters: 50 } },
+			'1000px':	{ range: '1024-1200', href: 'css/style-1000px.css', containers: 960, grid: { gutters: 25 }, viewport: { width: 1024 } },
+			'mobile':	{ range: '-1024', href: 'css/style-mobile.css', containers: '100%!', grid: { collapse: true, gutters: 20 }, viewport: { scalable: false } }
 		},
 		plugins: {
 			layers: {
